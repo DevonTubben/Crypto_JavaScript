e7e7054c66f1a5296f916435f231a933891141b9
Devon Tubben <tubbend1@msu.edu>
2021-05-17 12:24:42 -0400

    added url to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Checking")
+const BASE_URL = `https://api.coinlore.net/api/tickers/ (First 100 Coins)`
+
+function fetchCoins() { 
+fetch(BASE_URL)
+.then(res => res.json())
+.then(console.log)
+
+
+}
+
+function renderCoin(coin){ 
+    console.log(renderCoin)
+}
+
+fetchCoins()
