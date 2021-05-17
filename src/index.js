2c12aa0f92cdeeead742d36d2cbdb2bec7a3f733
Devon Tubben <tubbend1@msu.edu>
2021-05-17 13:48:34 -0400

    console prints loading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-const BASE_URL = `https://api.coinlore.net/api/tickers/ (First 100 Coins)`
-
-function fetchCoins() { 
-fetch(BASE_URL)
-.then(res => res.json())
-.then(console.log)
-
-
-}
-
-function renderCoin(coin){ 
-    console.log(renderCoin)
-}
-
-fetchCoins()
+console.log("Loading")
