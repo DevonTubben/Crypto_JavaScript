9d8eacd4b0d052dd150cf1f72b1d36e466cf5161
Devon Tubben <tubbend1@msu.edu>
2021-05-18 11:14:44 -0400

    corrected the api link

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Loading")
+const BASE_URL = `https://api.coinlore.net/api/tickers/`
+
+
+
+function fetchCoins() { 
+fetch(BASE_URL)
+.then(res => res.json())
+.then(console.log)
+
+
+}
+
+function renderCoin(coin){ 
+const coinContainer = document.getElementById("coin-list")
+const litag = document.createElement('li')
+const imageTag = document.createElement('img')
+imageTag.src = coin.url
+const pTag = document.createElement('p')
+pTag.innerText = coin.name
+liTag.append(imageTag, pTag)
+coinContainer.appendChild(liTag)
+}
+
+fetchCoins()
